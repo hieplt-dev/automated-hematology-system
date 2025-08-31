b13147384b69da694466cab41f4b3f602cadb263
hieplt-dev <lethanhhiep0220@gmail.com>
2025-08-31 08:11:05 +0700

    remove old src

--- utils/load_config.py
+++
@@ -1,6 +0,0 @@
-import yaml
-
-# load_config is a utility function to load configuration parameters from a YAML file.
-def load_config(path="config.yaml"):
-    with open(path, "r") as f:
-        return yaml.safe_load(f)
